56ccb5f3542bada1cf89cc3756f41ee8dab45708
Kitto <ruddn9040@gmail.com>
2024-04-08 15:35:06 +0900

    6주차 수업

--- Lecture06/main.js
+++ Lecture06/main.js
@@ -1,41 +1,40 @@
+//Canvas Element 불러오기
 var canvas = document.getElementById("GameScreenCanvas");
 var ctx = canvas.getContext("2d");
 
-
+var colors = ["#C7C5FF", "black", "blue", "magenta", "pink", "cyan", "orange"]; 
 
 class circle{
-    constructor(col, radius, positionX, positionY)
-    {
-        this.color = col;
+    constructor(colorIndex, radius, positionX, positionY){
+        this.color = colors[colorIndex];
         this.radius = radius;
         this.positionX = positionX;
         this.positionY = positionY;
     }
-    draw()
-    {
+    draw(){
+        ctx.save();
         ctx.beginPath();
-
-        for(var i=0; i<=360; i++)
-        {
-            ctx.lineTo(Math.cos(Math.PI/180*i),Math.sin(Math.PI/180*i));
-        }
 
         ctx.translate(this.positionX,this.positionY);
         ctx.scale(this.radius,this.radius);
-    
+
+        for(var i=0;i<360;i++){
+            ctx.lineTo(Math.cos(Math.PI/180*i), Math.sin(Math.PI/180*i));
+        }
+        
         ctx.fillStyle = this.color;
         ctx.fill();
-        ctx.closePath();
         
+        ctx.closePath();        
+        ctx.restore();
     }
-
 }
-function draw()
-{
-    var Circle = new circle("red",Math.random()*100, Math.random()*500, Math.random()*500);
+
+
+function render(){
+    var Circle = new circle(Math.round(Math.random()*6), Math.random()*100, Math.random()*500, Math.random()*500);
     Circle.draw();
-
-    requestAnimationFrame(draw);
+    requestAnimationFrame(render);
 }
 
-draw();
+render();
