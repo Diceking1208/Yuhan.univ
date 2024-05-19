0d5a3de209079087988ea7ebf9324625328d4e23
Diceking1208 <phy5840@naver.com>
2024-05-19 14:22:40 +0900

    10주차 과제

--- HW/Midterm/main.js
+++ HW/Midterm/main.js
@@ -4,18 +4,22 @@ var ctx = canvas.getContext("2d");
 // 게임시작
 document.getElementById('start-button').addEventListener('click', function() 
 {
-    setTimeout(draw, 1000);
+    startGame();
+    var playButton = document.querySelector('.playbutton');
+    playButton.style.display = 'none';
+});
 
-    var button = document.getElementById('start-button');
-    button.remove();
-    var h1Element = document.querySelector('#title');
-    h1Element.style.display = 'none';
+document.getElementById('restart-button').addEventListener('click', function() 
+{
+    restartGame();
+    var dieScreen = document.querySelector('.die');
+    dieScreen.style.display = 'none';
 });
 
 var canvasCenterX = canvas.width / 2;
 var canvasCenterY = canvas.height / 2;
 
-class Hexagon //플레이어
+class Hexagon 
 {
     constructor(posX, posY, rot, rotSpeed, size, color, moveDirX, moveDirY, speed) 
     {
@@ -29,6 +33,10 @@ class Hexagon //플레이어
         this.moveDirectionX = moveDirX;
         this.moveDirectionY = moveDirY;
         this.moveSpeed = speed;
+
+        this.lifeTextOffsetX = -250; //life 텍스트위치
+        this.lifeTextOffsetY = -350;
+        this.life = 3;
     }
 
     update() 
@@ -36,6 +44,26 @@ class Hexagon //플레이어
         this.position_X += (this.moveDirectionX * this.moveSpeed);
         this.position_Y += (this.moveDirectionY * this.moveSpeed);
         this.rotation += this.rotation_Speed;
+
+        for (var i = 0; i < monsters.length; i++) 
+        {
+            let monster = monsters[i];
+            let deltaX = this.position_X - monster.position_X;
+            let deltaY = this.position_Y - monster.position_Y;
+            let distance = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
+
+            if (distance < this.size + monster.size) 
+            {
+                this.life--;
+                monsters.splice(i, 1); // 닿으면 몬스터삭제
+                i--;
+            }
+        }
+
+        if (this.life <= 0) 
+        {
+            endGame();
+        }
     }
 
     draw() 
@@ -43,16 +71,22 @@ class Hexagon //플레이어
         ctx.beginPath();
         for (var i = this.rotation; i <= 360 + this.rotation; i += 72) 
         {
-            ctx.lineTo((Math.cos(Math.PI / 180 * i)) * this.size + this.position_X, (Math.sin(Math.PI / 180 * i)) * this.size + this.position_Y);
+            ctx.lineTo((Math.cos(Math.PI / 180 * i)) * this.size + this.position_X, (Math.sin(Math.PI / 180 * i)) * this.size + this.position_Y); //플레이어 그리기
         }
         ctx.fillStyle = this.color;
         ctx.fill();
         ctx.stroke();
         ctx.closePath();
+
+        ctx.font = "20px Arial";
+        ctx.fillStyle = "white";
+        var lifeText = "Lifes: " + this.life;
+        var textWidth = ctx.measureText(lifeText).width;
+        ctx.fillText(lifeText, this.position_X - textWidth / 2 + this.lifeTextOffsetX, this.position_Y - this.size + this.lifeTextOffsetY); //텍스트위치 고정
     }
 }
 
-class Monster //몬스터
+class Monster 
 {
     constructor(posX, posY, rot, rotSpeed, size, color, moveDirX, moveDirY, speed) 
     {
@@ -97,8 +131,7 @@ class Monster //몬스터
         ctx.beginPath();
         for (var i = 0; i <= 360; i += 10) 
         {
-            ctx.lineTo
-            (
+            ctx.lineTo(
                 Math.cos((Math.PI / 180) * i) * this.size + this.position_X,
                 Math.sin((Math.PI / 180) * i) * this.size + this.position_Y
             );
@@ -110,39 +143,71 @@ class Monster //몬스터
     }
 }
 
-var colors = ["#C7C5FF", "black", "blue", "magenta", "pink", "cyan", "orange"]; 
+var colors = ["#C7C5FF", "black", "blue", "magenta", "pink", "cyan", "orange"];
+var a;
+var monsters;
+var gameActive = false;
 
-var a = new Hexagon(canvasCenterX, canvasCenterY, -90.0, 2.0, 30.0, "green", 1.0, 1.0, 0.0);
-var monsters = [];
-
-// 오른쪽 방향 몬스터 생성
-for (var i = 0; i < 5; i++) 
+function startGame() 
 {
-    monsters.push(new Monster(canvasCenterX + 500, Math.random() * canvas.height, -90.0, 2.0, 10.0, Math.round(Math.random() * 6), 1.0, 1.0, 0.0));
+    a = new Hexagon(canvasCenterX, canvasCenterY, -90.0, 2.0, 30.0, "green", 1.0, 1.0, 0.0);
+    monsters = [];
+
+    // 오른쪽 방향 몬스터 생성
+    for (var i = 0; i < 5; i++) 
+    {
+        monsters.push(new Monster(canvasCenterX + 500, Math.random() * canvas.height, -90.0, 2.0, 10.0, Math.round(Math.random() * 6), 1.0, 1.0, 0.0));
+    }
+
+    // 왼쪽 방향 몬스터 생성
+    for (var i = 0; i < 5; i++) 
+    {
+        monsters.push(new Monster(canvasCenterX - 500, Math.random() * canvas.height, -90.0, 2.0, 10.0, Math.round(Math.random() * 6), -1.0, 1.0, 0.0));
+    }
+
+    // 위쪽 방향 몬스터 생성
+    for (var i = 0; i < 5; i++) 
+    {
+        monsters.push(new Monster(Math.random() * canvas.width, canvasCenterY - 500, -90.0, 2.0, 10.0, Math.round(Math.random() * 6), 1.0, -1.0, 0.0));
+    }
+
+    // 아래쪽 방향 몬스터 생성
+    for (var i = 0; i < 5; i++) 
+    {
+        monsters.push(new Monster(Math.random() * canvas.width, canvasCenterY + 500, -90.0, 2.0, 10.0, Math.round(Math.random() * 6), 1.0, 1.0, 0.0));
+    }
+
+    gameActive = true;
+    draw();
 }
 
-// 왼쪽 방향 몬스터 생성
-for (var i = 0; i < 5; i++) 
+function endGame() //죽음
 {
-    monsters.push(new Monster(canvasCenterX - 500, Math.random() * canvas.height, -90.0, 2.0, 10.0, Math.round(Math.random() * 6), -1.0, 1.0, 0.0));
+    gameActive = false;
+    var dieScreen = document.querySelector('.die');
+    dieScreen.style.display = 'block';
 }
 
-// 위쪽 방향 몬스터 생성
-for (var i = 0; i < 5; i++) 
+function restartGame() //리스타트
 {
-    monsters.push(new Monster(Math.random() * canvas.width, canvasCenterY - 500, -90.0, 2.0, 10.0, Math.round(Math.random() * 6), 1.0, -1.0, 0.0));
-}
-
-// 아래쪽 방향 몬스터 생성
-for (var i = 0; i < 5; i++) 
-{
-    monsters.push(new Monster(Math.random() * canvas.width, canvasCenterY + 500, -90.0, 2.0, 10.0, Math.round(Math.random() * 6), 1.0, 1.0, 0.0));
+    startGame();
+    var playButton = document.querySelector('.playbutton');
+    playButton.style.display = 'none';
+    var dieScreen = document.querySelector('.die');
+    dieScreen.style.display = 'none';
 }
 
 function draw() 
 {
+    if (!gameActive) 
+    {
+        ctx.fillStyle = 'red';
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+        return;
+    }
+
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    
+
     a.update();
     for (var i = 0; i < monsters.length; i++) 
     {
@@ -151,7 +216,7 @@ function draw()
 
     ctx.save();
     ctx.translate(canvasCenterX - a.position_X, canvasCenterY - a.position_Y);
-    
+
     a.draw();
     for (var i = 0; i < monsters.length; i++) 
     {
@@ -165,23 +230,24 @@ function draw()
 
 function GetKey(event) 
 {
-    if (event.key ==='ArrowRight') 
+    if (!gameActive) return; // 게임 오버 상태에서는 키 입력을 무시
+
+    if (event.key === 'ArrowRight') 
     {
         a.position_X += 5;
     }
-    if (event.key ==='ArrowLeft')
+    if (event.key === 'ArrowLeft') 
     {
         a.position_X -= 5;
     }
-    if (event.key ==='ArrowUp')
+    if (event.key === 'ArrowUp') 
     {
         a.position_Y -= 5;
     }
-    if (event.key ==='ArrowDown') 
+    if (event.key === 'ArrowDown') 
     {
         a.position_Y += 5;
     }
 }
 
 document.addEventListener('keydown', GetKey);
-
